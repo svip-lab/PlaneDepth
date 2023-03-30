716406830d2f1de53c4cc70fc88ae59516e59002
Ruoyu Wang <wangry3@shanghaitech.edu.cn>
2023-03-30 14:56:16 +0800

    add the MLL flag

--- eval.sh
+++ eval.sh
@@ -4,5 +4,7 @@ CUDA_VISIBLE_DEVICES=0 python evaluate_depth_HR.py \
 --models_to_load encoder depth \
 --use_denseaspp \
 --plane_residual \
+--use_mixture_loss \
+--batch_size 1 \
 --width 1280 \
 --height 384
